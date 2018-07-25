3fb500c705e47bf74c9b480f927485d0e7afa0d8
Ingrid plazas <ingridviv17@gmail.com>
2018-07-24 21:24:47 -0500

    Aplicaciondepruebas

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
+declare var google;
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,27 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  @ViewChild('map') mapElement;
+  map: any;
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  ionViewDidLoad(){
+    this.initMap();
+  }
+  initMap(){
+
+    let latLng =new google.maps.LatLng(-34.9290,138.6010); 
+
+    let mapOptions={
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    this.map=google.maps.Map(this.mapElement.nativeElement,mapOptions);
   }
 
 }
