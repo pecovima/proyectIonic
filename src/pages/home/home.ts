71997f52e90c5a95c60e13fc8d2d47171e3b93b8
Ingrid plazas <ingridviv17@gmail.com>
2018-07-26 20:57:46 -0500

    Aplicaciondepruebas

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,35 +1,86 @@
-import { Component,ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
-declare var google;
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  @ViewChild('map') mapElement;
-  map: any;
-
-  constructor(public navCtrl: NavController) {
+usuario : string="";
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
 
   }
 
-  ionViewDidLoad(){
-    this.initMap();
+  showAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Login',
+      subTitle: 'Sesión iniciada...',
+      buttons: ['OK']
+    });
+    alert.present();
   }
-  initMap(){
 
-    let latLng =new google.maps.LatLng(-34.9290,138.6010); 
+  showAlertRecuperar() {
+    let alert = this.alertCtrl.create({
+      title: 'Recuperar Contraseña',
+      subTitle: 'Escribe tu correo para recuperar tu contraseña.',
+      inputs : [{
+        placeholder: 'Correo electrónico'
+        },
+        {
+          placeholder: 'Correo electrónico2'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Enviar correo',
+          handler: data => {
+            console.log(data);
+            this.showAlertConfirmaRecuperar(data);
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
 
-    let mapOptions={
-      center: latLng,
-      zoom: 15,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-
-    this.map=google.maps.Map(this.mapElement.nativeElement,mapOptions);
+  showAlertConfirmaRecuperar(data) {
+    let alert = this.alertCtrl.create({
+      title: 'Recuperar Contraseña',
+      subTitle: 'Acabamos de enviarte un mensaje a tu correo: ' + data[0] + ', ' + data[1],
+      buttons: [  {
+        text: 'OK',
+        handler: seg => {
+          console.log(seg);
+          this.showConfirmCorr(seg);
+        }
+      }]
+    });
+    alert.present();
+  }
+  showConfirmCorr(seg) {
+    const confirm = this.alertCtrl.create({
+      title: 'Este es su correo y contraseña?',
+      message: 'Desea comfirmar su correo y contraseña?',
+      buttons: [
+        {
+          text: 'Atras',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        },
+        {
+          text: 'Continuar',
+          handler: () => {
+            console.log('Agree clicked');
+          }
+        }
+      ]
+    });
+    confirm.present();
   }
 
 }
+
+
